920640d1c6339aed4b6fd85e47ae5404806142e3
Fasamii <fasami0512@gmail.com>
2026-05-29 16:00:18 +0200

    appearance remove use

--- clients/desktop/src/appearance.rs
+++ clients/desktop/src/appearance.rs
@@ -1,13 +1,7 @@
-use egui::{
-    Color32, Stroke, Visuals,
-    style::{Selection, WidgetVisuals, Widgets},
-};
-
-pub const FG: Color32 = Color32::WHITE;
-pub const BG: Color32 = Color32::from_rgba_premultiplied(0, 0, 0, 255);
-// TODO: Make that alpha configurable
-pub const ASIDE_BG: Color32 = Color32::from_rgba_premultiplied(20, 20, 20, 160);
-pub const BORDER: Color32 = Color32::from_gray(100);
+pub const FG: egui::Color32 = egui::Color32::WHITE;
+pub const BG: egui::Color32 = egui::Color32::from_rgba_premultiplied(0, 0, 0, 255);
+pub const ASIDE_BG: egui::Color32 = egui::Color32::from_rgba_premultiplied(20, 20, 20, 160);
+pub const BORDER: egui::Color32 = egui::Color32::from_gray(100);
 
 pub const BORDER_WIDTH: f32 = 1.2;
 
@@ -23,38 +17,40 @@ pub const BUTTON_BIG: f32 = 44.0;
 pub const BUTTON_BIG_V2: [f32; 2] = [BUTTON_BIG, BUTTON_BIG];
 
 pub fn apply(cc: &eframe::CreationContext) {
-    let mut v = Visuals::dark();
+    let mut v = egui::Visuals::dark();
     v.panel_fill = BG;
     v.window_shadow = egui::Shadow::NONE;
     v.popup_shadow = egui::Shadow::NONE;
-    v.window_stroke = Stroke::new(BORDER_WIDTH, BORDER);
+    v.window_stroke = egui::Stroke::new(BORDER_WIDTH, BORDER);
 
-    v.selection = Selection {
+    v.selection = egui::style::Selection {
         bg_fill: FG,
-        stroke: Stroke::new(1.0, BG),
+        stroke: egui::Stroke::new(1.0, BG),
     };
 
-    let flat =
-        |bg_fill: Color32, weak_bg_fill: Color32, fg: Color32, border: Color32| WidgetVisuals {
-            bg_fill,
-            weak_bg_fill,
-            fg_stroke: Stroke::new(BORDER_WIDTH, fg),
-            bg_stroke: Stroke::new(BORDER_WIDTH, border),
-            corner_radius: egui::CornerRadius::ZERO,
-            expansion: 0.0,
-        };
+    let flat = |bg_fill: egui::Color32,
+                weak_bg_fill: egui::Color32,
+                fg: egui::Color32,
+                border: egui::Color32| egui::style::WidgetVisuals {
+        bg_fill,
+        weak_bg_fill,
+        fg_stroke: egui::Stroke::new(BORDER_WIDTH, fg),
+        bg_stroke: egui::Stroke::new(BORDER_WIDTH, border),
+        corner_radius: egui::CornerRadius::ZERO,
+        expansion: 0.0,
+    };
 
-    v.widgets = Widgets {
-        noninteractive: WidgetVisuals {
+    v.widgets = egui::style::Widgets {
+        noninteractive: egui::style::WidgetVisuals {
             bg_fill: BG,
             weak_bg_fill: BG,
-            bg_stroke: Stroke::new(BORDER_WIDTH, BORDER),
-            fg_stroke: Stroke::new(1.0, FG),
+            bg_stroke: egui::Stroke::new(BORDER_WIDTH, BORDER),
+            fg_stroke: egui::Stroke::new(1.0, FG),
             corner_radius: egui::CornerRadius::ZERO,
             expansion: 0.0,
         },
-        inactive: flat(Color32::from_gray(140), BG, FG, BORDER),
-        hovered: flat(Color32::from_gray(180), BORDER, FG, BORDER),
+        inactive: flat(egui::Color32::from_gray(140), BG, FG, BORDER),
+        hovered: flat(egui::Color32::from_gray(180), BORDER, FG, BORDER),
         active: flat(FG, FG, FG, BORDER),
         open: flat(FG, FG, FG, FG),
     };
